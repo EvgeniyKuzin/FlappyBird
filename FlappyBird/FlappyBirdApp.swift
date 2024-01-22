e3f341d2fa0c05194327261935524db970a9ac3b
EvgeniyKuzin <vaglover@yandex.ru>
2024-01-23 00:13:33 +0300

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Evgeniy Kuzin on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
