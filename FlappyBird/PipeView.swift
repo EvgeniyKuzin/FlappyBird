435e39cb97403cc6af10937c5e741b8fa1520b65
EvgeniyKuzin <vaglover@yandex.ru>
2024-01-23 01:18:03 +0300

    minor changes

---
+++ FlappyBird/PipeView.swift
@@ -0,0 +1,18 @@
+//
+//  PipeView.swift
+//  FlappyBird
+//
+//  Created by Evgeniy Kuzin on 23.01.2024.
+//
+
+import SwiftUI
+
+struct PipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PipeView()
+}
---
+++ FlappyBird/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  FlappyBird
+//
+//  Created by Evgeniy Kuzin on 23.01.2024.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
